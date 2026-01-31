718f13e84889e4e8c2fcbf79762bd1bb055dd7ae
Jordan Welch <jordanhwelch@gmail.com>
2026-01-30 20:11:22 -0600

    install html parser

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,22 @@
 import { defineConfig } from 'eslint/config';
 import mmm from '@jhwelch/mmm-eslint-config';
+import html from '@html-eslint/eslint-plugin';
 
 export default defineConfig([
-  ...mmm,
+  ...mmm.map(config => ({
+    files: ['**/*.js', '**/*.mjs'],
+    ...config,
+  })),
+  {
+    files: ['**/*.html'],
+    plugins: {
+      html,
+    },
+    extends: ['html/recommended'],
+    language: 'html/html',
+    rules: {
+      'html/no-duplicate-class': 'error',
+      'html/indent': ['error', 2],
+    },
+  },
 ]);
