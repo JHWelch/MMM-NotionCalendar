c7d26fa96319c1ce37100d8e65997db7efdafa2a
Jordan Welch <jordanhwelch@gmail.com>
2026-01-30 18:23:26 -0600

    "Magic Mirror" -> "MagicMirror²"

--- MMM-NotionCalendar.js
+++ MMM-NotionCalendar.js
@@ -1,6 +1,6 @@
 /* global Module */
 
-/* Magic Mirror
+/* MagicMirror²
  * Module: MMM-NotionCalendar
  *
  * By Jordan Welch
--- node_helper.js
+++ node_helper.js
@@ -1,4 +1,4 @@
-/* Magic Mirror
+/* MagicMirror²
  * Node Helper: MMM-NotionCalendar
  *
  * By Jordan Welch
